5d89ca2bcd49c346813dbbe0c397e0f707641783
Suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2016-01-07 17:16:23 +0100

    close #8

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ doSomething = function (){
 	var stage = new PIXI.Stage(0x66FF99);
 	
 	// create a renderer instance
-	var renderer = PIXI.autoDetectRenderer(400, 300);
+	var renderer = PIXI.autoDetectRenderer(640, 500);
 	
 	// add the renderer view element to the DOM
 	document.body.appendChild(renderer.view);
+    
+    Graphics = PIXI.Graphics;
 	
 	requestAnimationFrame( animate );
 	
@@ -36,5 +38,25 @@ doSomething = function (){
 	    // render the stage   
 	    renderer.render(stage);
 	}
+    
+// create a board with tiles and set click event for each of them.
+for (var i = 6; i >= 0; i--) {
+    for (var j = 5; j >= 0; j--) {
+        var tile = new Graphics();
+        tile.beginFill(0xffffff);
+        tile.drawRect(110 + 60 * i, 80 + 55 * j, 50, 50);
+        tile.endFill();
+        tile.buttonMode = true;
+        tile.interactive = true;
+        tile.val = j + "-" + i;
+        tile.on('mousedown', onTilesClick);
+        stage.addChild(tile);
+    }
+}
+    function onTilesClick()
+    {
+    }
+    
+    
 
 };
