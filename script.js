b98642b33239e3285ff1a36a1dc0b5e4c14e37c3
Suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2016-01-07 17:20:30 +0100

    remove the bunny

--- script.js
+++ script.js
@@ -13,28 +13,11 @@ doSomething = function (){
 	
 	requestAnimationFrame( animate );
 	
-	// create a texture from an image path
-	var texture = PIXI.Texture.fromImage("resourses/bunny.png");
-	// create a new Sprite using the texture
-	var bunny = new PIXI.Sprite(texture);
-	
-	// center the sprites anchor point
-	bunny.anchor.x = 0.5;
-	bunny.anchor.y = 0.5;
-	
-	// move the sprite t the center of the screen
-	bunny.position.x = 200;
-	bunny.position.y = 150;
-	
-	stage.addChild(bunny);
 	
 	function animate() {
 	
 	    requestAnimationFrame( animate );
 	
-	    // just for fun, lets rotate mr rabbit a little
-	    bunny.rotation += 0.1;
-		
 	    // render the stage   
 	    renderer.render(stage);
 	}
